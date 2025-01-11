b92a68099e2d29059c6a2a72262cbba999f1043d
monsafali <monsafali272@gmail.com>
2025-01-11 08:49:03 +0500

    my solution

---
+++ 03-Reviews/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Reviews from "./MYSolution/Reviews";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h2 className="mb-4">OUR REViews</h2>
+        <Reviews />
+      </div>
+    </>
+  );
+}
+
+export default App;
